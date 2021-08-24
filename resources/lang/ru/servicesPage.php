37d5274691b8be73b45e92dd459d7f8daa1035c4
Bilel <bilell19@gmail.com>
2021-08-24 15:08:11 +0300

    add ru and remove ar language

---
+++ resources/lang/ru/servicesPage.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'fine_touch_of_luxury' => 'Fine Touch Of Luxury',
+    'fine_touch_of_luxury_details' => 'Fine Touch Of Luxury details',
+    'virtual_tour_360' => 'Virtual Tour 360',
+    'virtual_tour_360_details' => 'Virtual Tour 360 details',
+    'our_process' => 'Our Approach',
+    'our_process_details' => 'Our Approach details',
+];
